e93232f913c35325d5e68e99ec67d6ae6dd04ee8
Victor Pontis <vpontis@gmail.com>
2013-10-18 16:09:51 -0400

    initial commit with rails new and docs folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VpontisEmzhangTscizzleProj3::Application.config.secret_key_base = '2ab45c7a4da6eed04c0b24f5873daba60cbda4fde2e4c605f73530f8ddbf6bca1bb80ab2eb704b20b97ba068fcbe7c7e2ee3c006be1170a658dc680f2411b796'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VpontisEmzhangTscizzleProj3::Application.config.session_store :cookie_store, key: '_vpontis_emzhang_tscizzle_proj3_session'
