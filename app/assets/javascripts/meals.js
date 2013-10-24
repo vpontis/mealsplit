c72425b0bb9638b2a060efe9bf9ef66394a4db04
Victor Pontis <vpontis@gmail.com>
2013-10-24 16:38:21 -0400

    added js to add new meal participant label

--- app/assets/javascripts/meals.js
+++ app/assets/javascripts/meals.js
@@ -3,7 +3,20 @@ var populate_typeahead = function(){
     {
       name: 'name',
       remote: '/load_suggestions.json',
-      valueKey: 'name',
-      
+      valueKey: 'name',    
     }]);
 };
+
+var meal_participants = 1;
+var add_meal_participant = function() {
+  meal_participants += 1
+  var new_meal_participant_id = 'meal_participant_' + meal_participants;
+  var new_meal_participant = $('#meal-participant-1').clone();
+  $(new_meal_participant).attr('id', '');
+  $(new_meal_participant).find('label').attr('for', new_meal_participant_id);
+  $(new_meal_participant).find('input')
+    .attr('name', new_meal_participant_id)
+    .attr('id', new_meal_participant_id);
+  $(new_meal_participant).insertBefore('#add-participant-button');
+  return new_meal_participant;
+}
--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -4,16 +4,26 @@ class MealsController < ApplicationController
   end
 
   def restaurant_choice
-    restaurant_name = params[:restaurant_name]
-    redirect_to '/get_participants'
+    restaurant = Restaurant.find_by(name: params[:restaurant_name])
+    if restaurant.nil?
+      flash[:danger] = "You need to choose the name of an existing restaurant."
+      redirect_to new_meal_path
+    else
+      cookies[:restaurant_name] = restaurant.name
+      redirect_to '/get_participants'
+    end
   end
 
   def get_participants
-    
+    if cookies[:restaurant_name].nil?
+      flash[:danger] = "You need to choose a restaurant before you choose particpants."
+      redirect_to root_path
+    end
+    @restaurant_name = cookies[:restaurant_name]
   end
 
   def participants_list
-
+    
   end
 
   def index
