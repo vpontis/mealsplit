75ef37a9e2a648b7ed24b5ddfcbc4bcb77ca9e4d
Emily Zhang <1800.ehz.hang@gmail.com>
2013-10-26 14:29:32 -0400

    more fake restaurant data

--- app/assets/javascripts/meals.js
+++ app/assets/javascripts/meals.js
@@ -2,7 +2,7 @@ var populate_typeahead = function(){
   $('#auto_complete_restaurant').typeahead([      
     {
       name: 'name',
-      remote: '/load_suggestions.json',
+      prefetch: '/load_suggestions.json',
       valueKey: 'name',    
     }]);
 };
