85f4c4f08b396ebbfff137d5f6be3a29daff6343
Emily Zhang <1800.ehz.hang@gmail.com>
2013-10-30 19:20:02 -0400

    new meal test

--- test/models/meal_test.rb
+++ test/models/meal_test.rb
@@ -4,4 +4,10 @@ class MealTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test test_payer do
+  	meal = FactoryGirl.create(:meal)
+  	meal.payer_id = nil
+    assert_nil 
+  end
 end
