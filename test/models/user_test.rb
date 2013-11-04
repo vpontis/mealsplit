9e6bfae7ac331db858bbaf8a6bc070c8832a8116
Emily Zhang <1800.ehz.hang@gmail.com>
2013-11-03 19:48:36 -0500

    user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -44,7 +44,9 @@ class UserTest < ActiveSupport::TestCase
   def test_owes1
     restaurant = FactoryGirl.create(:restaurant)
     meal1 = FactoryGirl.create(:meal, restaurant: restaurant)
-    participant = FactoryGirl.create(:participant, email: "victor@me.com", name: "Victor", meal: meal1)
+    food_item1 = FactoryGirl.build(:food_item, cost: 10.0,)
+    food_item2 = FactoryGirl.build(:food_item,  cost: 11.0,)
+    participant = FactoryGirl.create(:participant, email: "victor@me.com", name: "Victor", meal: meal1, food_items: [food_item1, food_item2])
     user = FactoryGirl.build(:user, name: "Victor", email: "victor@me.com", password: "fdsafdsa")
     assert_equal meal1, user.meals[0]
 
